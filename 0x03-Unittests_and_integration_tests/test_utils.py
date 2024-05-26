83d5d38b992f23678d8e4d1453f81ebc6d41cb18
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-26 09:15:45 -0700

    more tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 from unittest.mock import patch, Mock
-from utils import get_json
+from utils import get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -85,6 +85,49 @@ class TestMemoize(unittest.TestCase):
             # Check that the results are correct
             self.assertEqual(result1, 42)
             self.assertEqual(result2, 42)
+
+
+class GithubOrgClient:
+    """ GithubOrgclient class """
+    ORG_URL = "https://api.github.com/orgs/{org}"
+
+    def __init__(self, org_name: str) -> None:
+        """ Instantiates a class """
+        self._org_name = org_name
+
+    @memoize
+    def org(self) -> dict:
+        """ Memoize org """
+        return get_json(self.ORG_URL.format(org=self._org_name))
+
+    @property
+    def _public_repos_url(self) -> str:
+        """ gets pub repos """
+        return self.org["repos_url"]
+
+    @memoize
+    def repos_payload(self) -> dict:
+        """ repos payload """
+        return get_json(self._public_repos_url)
+
+    def public_repos(self, license: str = None) -> list:
+        """ Checks pub repos """
+        json_payload = self.repos_payload
+        public_repos = [
+            repo["name"] for repo in json_payload
+            if license is None or self.has_license(repo, license)
+        ]
+        return public_repos
+
+    @staticmethod
+    def has_license(repo: dict, license_key: str) -> bool:
+        """ Checks for license """
+        assert license_key is not None, "license_key cannot be None"
+        try:
+            has_license = repo["license"]["key"] == license_key
+        except KeyError:
+            return False
+        return has_license
 
 
 if __name__ == '__main__':

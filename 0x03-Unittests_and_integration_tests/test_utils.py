c5e67399ddab5d56519c831cd855c0671a0f0325
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-26 09:24:28 -0700

    more tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from unittest.mock import patch, Mock
 from utils import get_json, memoize
+from client import GithubOrgClient
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -87,47 +88,23 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(result2, 42)
 
 
-class GithubOrgClient:
-    """ GithubOrgclient class """
-    ORG_URL = "https://api.github.com/orgs/{org}"
+class TestGithubOrgClient(unittest.TestCase):
+    """ TestGithubOrgClient """
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """ Test Org """
+        test_payload = {"payload": True}
+        mock_get_json.return_value = test_payload
 
-    def __init__(self, org_name: str) -> None:
-        """ Instantiates a class """
-        self._org_name = org_name
+        client = GithubOrgClient(org_name)
+        result = client.org
 
-    @memoize
-    def org(self) -> dict:
-        """ Memoize org """
-        return get_json(self.ORG_URL.format(org=self._org_name))
-
-    @property
-    def _public_repos_url(self) -> str:
-        """ gets pub repos """
-        return self.org["repos_url"]
-
-    @memoize
-    def repos_payload(self) -> dict:
-        """ repos payload """
-        return get_json(self._public_repos_url)
-
-    def public_repos(self, license: str = None) -> list:
-        """ Checks pub repos """
-        json_payload = self.repos_payload
-        public_repos = [
-            repo["name"] for repo in json_payload
-            if license is None or self.has_license(repo, license)
-        ]
-        return public_repos
-
-    @staticmethod
-    def has_license(repo: dict, license_key: str) -> bool:
-        """ Checks for license """
-        assert license_key is not None, "license_key cannot be None"
-        try:
-            has_license = repo["license"]["key"] == license_key
-        except KeyError:
-            return False
-        return has_license
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':

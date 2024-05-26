904be74ac45d5852fd5b13fc6b0eb3c3ce788ba9
nenyeonyema <chinenyeonyema1@gmail.com>
2024-05-26 09:47:02 -0700

    more tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-from unittest.mock import patch
+from unittest.mock import patch, Mock, PropertyMock
 from utils import get_json, memoize
 from client import GithubOrgClient
 
@@ -77,9 +77,49 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, test_payload)
 
+        def test_public_repos_url(self):
+        with patch('client.GithubOrgClient.org', new_callable=PropertyMock) as mock_org:
+            test_payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
+            mock_org.return_value = test_payload
+
+            client = GithubOrgClient("google")
+            result = client._public_repos_url
+
+            self.assertEqual(result, test_payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ Test pub reops """
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/google/repos"
+
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+
+            self.assertEqual(result, ["repo1", "repo2", "repo3"])
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Test license """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 class TestMemoize(unittest.TestCase):
     """ Test Memoize """
+
     def test_memoize(self):
         """ Test Memoize """
         class TestClass:
